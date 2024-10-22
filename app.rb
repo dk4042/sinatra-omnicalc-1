19449ddaf8ff7641b8fc36c93f7f6e4d4fcd6ae9
dk4042 <dkhamaza@uchicago.edu>
2024-10-22 07:24:33 +0000

    b

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
 get("/square/new") do
   erb(:new_square_calc)
 end
@@ -11,14 +18,11 @@ get ("/square/results") do
   erb(:square_results)
 end
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
 
 get("/square_root/new") do
+  erb(:new_square_calc)
+
+  get9"/square_root/results") do
   @the_num = params.fetch("users_number").to_f
   @the_result = @the_num ** (0.5)
   erb(:square_root_result)
